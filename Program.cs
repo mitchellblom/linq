be8dd27e4bd8f1fad6fd06e96ab97813095012f7
Mitchell Blom <mitchellblom@gmail.com>
2017-07-11 15:15:50 -0500

    four and six multiples

--- Program.cs
+++ Program.cs
@@ -8,7 +8,6 @@ namespace linq
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Linqqq!");
             // Find the words in the collection that start with the letter 'L'
             List<string> fruits = new List<string>() {"Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry"};
 
@@ -20,24 +19,19 @@ namespace linq
                 Console.WriteLine($"{fruit}");
             }
 
-            // var LFruits = from fruit in fruits ...
-            // // Which of the following numbers are multiples of 4 or 6
-            // List<int> numbers = new List<int>()
-            // {
-            //     15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
-            // }
+            // Which of the following numbers are multiples of 4 or 6
+            List<int> numbers = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
 
-            // var fourSixMultiples = numbers.Where();
+            var fourSixMultiples = numbers.Where(number => (number%4 == 0) || (number%6 == 0));
+
+            foreach (int number in fourSixMultiples) {
+                Console.WriteLine($"{number}");
+            }
 
         }
     }
 
 }
-
-// XXXXXXXX
-
-//         // Display each number that was the acceptable size
-//         foreach (int c in idealSizes)
-//         {
-//             Console.WriteLine($"{c}");
-//         }
