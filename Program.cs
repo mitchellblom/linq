523338cc173dfadfa3914334d2ba000a8aa1267b
Mitchell Blom <mitchellblom@gmail.com>
2017-07-11 13:07:03 -0500

    first and catching up c# repos

--- Program.cs
+++ Program.cs
@@ -9,4 +9,22 @@ namespace linq
             Console.WriteLine("Hello World!");
         }
     }
+
+    // Find the words in the collection that start with the letter 'L'
+    List<string> fruits = new List<string>() {'Lemon', 'Apple', 'Orange', 'Lime', 'Watermelon', 'Loganberry'};
+
+    var LFruits = from fruit in fruits ...
+    // Which of the following numbers are multiples of 4 or 6
+    List<int> numbers = new List<int>()
+    {
+        15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+    }
+
+    var fourSixMultiples = numbers.Where();
+
+
+
+
+
+
 }
