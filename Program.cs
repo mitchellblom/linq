c6ecad4ce3dc75ad212f4354413791d157a6f46e
Mitchell Blom <mitchellblom@gmail.com>
2017-07-11 14:03:55 -0500

    L words in collection

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace linq
 {
@@ -6,25 +8,36 @@ namespace linq
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Linqqq!");
+            // Find the words in the collection that start with the letter 'L'
+            List<string> fruits = new List<string>() {"Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry"};
+
+            IEnumerable<string> startsWithL = from fruit in fruits
+                where fruit.StartsWith("L")
+                select fruit;
+
+            foreach (string fruit in startsWithL) {
+                Console.WriteLine($"{fruit}");
+            }
+
+            // var LFruits = from fruit in fruits ...
+            // // Which of the following numbers are multiples of 4 or 6
+            // List<int> numbers = new List<int>()
+            // {
+            //     15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            // }
+
+            // var fourSixMultiples = numbers.Where();
+
         }
     }
 
-    // Find the words in the collection that start with the letter 'L'
-    List<string> fruits = new List<string>() {'Lemon', 'Apple', 'Orange', 'Lime', 'Watermelon', 'Loganberry'};
-
-    var LFruits = from fruit in fruits ...
-    // Which of the following numbers are multiples of 4 or 6
-    List<int> numbers = new List<int>()
-    {
-        15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
-    }
-
-    var fourSixMultiples = numbers.Where();
-
-
-
-
-
-
 }
+
+// XXXXXXXX
+
+//         // Display each number that was the acceptable size
+//         foreach (int c in idealSizes)
+//         {
+//             Console.WriteLine($"{c}");
+//         }
