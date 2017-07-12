9996c4ba18807ae75bb8c6d66d92a9390019ef8d
Mitchell Blom <mitchellblom@gmail.com>
2017-07-12 10:25:11 -0500

    millionaire list and count by bank

--- Program.cs
+++ Program.cs
@@ -93,7 +93,74 @@ namespace linq
             var maxDouble = prices.Max();
             Console.WriteLine(maxDouble);
 
+        // Partitioning Operations
+
+            // //Store each number in the following List until a perfect square is detected.
+            List<int> wheresSquaredo = new List<int>()
+            {
+                66, 12, 8, 27, 82, 34, 7, 50, 19, 46, 81, 23, 30, 4, 68, 14
+            };
+
+            // // foreach (int number in ascendingNumbers) {
+            // //     wheresSquaredo.Add(number);
+            // //     double squareRoot = Math.Sqrt(number);
+            // //     Console.WriteLine(squareRoot);
+            // //     if (number%squareRoot == squareRoot) {
+            // //         break;
+            // //     }
+            // // }
+
+            // // Build a collection of customers who are millionaires
+            // // Given the same customer set, display how many millionaires per bank.
+
+            List<Customer> customers = new List<Customer>() {
+                new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+                new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+                new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+                new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+                new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+                new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+                new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+                new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+                new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+                new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+            };
+
+            var millionaires = from customer in customers // can use var for linq statements in IEnumerable lists
+                where customer.Balance >= 1000000
+                select customer;
+
+            foreach (var customer in millionaires)
+            {
+                Console.WriteLine($"{customer.Name} {customer.Balance}");
+            }
+
+            var millGroup = from millionaire in millionaires
+                group millionaire by millionaire.Bank into taco
+                select new { Bank = taco.Key, Customers = taco};
+
+            var grouped = customers.Where(c => c.Balance >= 1000000) // what goes into grouped is millionaire customers
+                .GroupBy(d => d.Bank);
+
+            foreach (var potato in grouped)
+            {
+                Console.WriteLine($"{potato.Key} {potato.Count()}");
+
+                foreach (var customer in potato) 
+                {
+                    Console.WriteLine($"   {customer.Name} {customer.Balance}");
+                }
+            }   
+
         }
+
+        public class Customer
+        {
+            public string Name { get; set; }
+            public double Balance { get; set; }
+            public string Bank { get; set; }
+        }
+
     }
 
 }
